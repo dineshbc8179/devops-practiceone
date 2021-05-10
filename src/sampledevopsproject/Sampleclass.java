6bcd4691b7ddb5ae0fa706c8e656ebd3a2337a80
dinesh1 <rokkystar03@gmail.com>
2021-05-10 08:15:04 +0530

    source code commit

---
+++ src/sampledevopsproject/Sampleclass.java
@@ -0,0 +1,11 @@
+package sampledevopsproject;
+
+public class Sampleclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("this is for devops-practice");
+
+	}
+
+}
